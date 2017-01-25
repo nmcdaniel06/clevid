db417558ce3593f424e65c5edb6601c40bf89d2a
Nathan <nathangmcdaniel@gmail.com>
2017-01-25 17:45:26 -0600

    Starting without database connection

--- php/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-
-    $conn = new mysqli("localhost", "root", "chevy454", "clevid") or die(mysqli_error());
-
-
-
-?>
--- test.php
+++
@@ -1,3 +0,0 @@
-<div class="page-header">
-  <h1>CLEVID <small>Web Development</small></h1>
-</div>
