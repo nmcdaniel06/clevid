26a8b390dd1a415fbcad391832e7d228f878df53
Nathan <nathangmcdaniel@gmail.com>
2017-01-25 18:38:38 -0600

    header completed

--- index.php
+++ index.php
@@ -2,39 +2,8 @@
 
 <!---Begin Body---------------------------------------------->
 
-<div class="row">
-
-  <div class="col-md-9">
 
 
-<?php
-
-
-
-    if((isset($_GET['page']))) {
-        $page = $_GET['page'];
-    }else {
-        $page = 'home';
-    }
-
-    $sql = "SELECT name, content FROM pages WHERE name='$page'";
-    $result = $conn->query($sql) or die(mysqli_error());
-
-    if($result) {
-
-        $row = $result->fetch_object();
-        //echo "<h2>" . $row->name . "</h2>";
-        echo $row->content;
-
-        }
-
-?>
-  </div><!--end left column------------------------------>
-  <div class="col-md-3">
-    <a href="https://www.shopify.com/?ref=nathan-mcdaniel"> <img src="pics/shopify-partner.jpg" class="img-responsive" alt="Responsive image"></a>
-
-    <a href="https://www.shopify.com/?ref=nathan-mcdaniel"> <img src="pics/smshopify.jpg" class="img-responsive" alt="Responsive image"></a>
-  </div><!--end right column----------------------------->
 
 
 
--- php/hat.php
+++ php/hat.php
@@ -18,40 +18,43 @@
     <![endif]-->
   </head>
   <body>
-    <div class="container">
-<!--header----------------------------->
 
-      <div class="page-header">
-        <h1>CLEVID <small>Web Development</small></h1>
-      </div>
-
-      <!-------------Navbar------------->
-
+    <!--Navbar--------------------------->
     <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="/clevid">Clevid</a>
-        <ul class="nav navbar-nav navbar-right">
+     <div class="container">
+       <div class="navbar-header">
+         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+           <span class="sr-only">Toggle navigation</span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+         </button>
+         <a class="navbar-brand" href="#">Clevid</a>
+       </div>
+       <div id="navbar" class="navbar-collapse collapse">
+         <ul class="nav navbar-nav navbar-right">
+           <li><a href="#home">Home</a></li>
+           <li><a href="#about">About</a></li>
+           <li><a href="#contact">Contact</a></li>
+         </ul>
+       </div><!--/.nav-collapse -->
+     </div>
+   </nav>
 
 
-          <?php
+    <div class="container">
+<!--header------------------------------->
+<!--------------------------------------->
 
-    require("php/connection.php");
-    $sql = "SELECT name, url, title FROM nav";
-    $result = $conn->query($sql) or die(mysqli_error());
+<div class="row">
+  <div class="col-md-4">
+    <img src="pics/logo.png">
+  </div><!--ColumnEnd-------------------->
+  <div class="col-md-8">
+    <br />
+    <h1>Web Design and Developement<br />
+    <small>Clever Ideas for your personal or business goals!</small></h1>
 
-    if($result) {
-
-        while($row = $result->fetch_object()) {
-
-            echo "<li><a href='{$row->url}' title='{$row->title}'>{$row->name}</a></li>";
-        }
-    }
-
-?>
-
-
-        </ul>
-      </div>
-    </nav>
-
-    <!-------------END---Navbar------------->
+  </div><!--ColumnEnd-------------------->
+</div><!--HeaderRow---------------------->
+<hr />
